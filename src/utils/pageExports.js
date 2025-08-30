b02c23eef74076bfeb1430002b31cc1ba62e8578
kakeru13468 <auchergod@gmail.com>
2025-08-30 14:16:04 +0800

    fix: fix page router error

--- src/utils/pageExports.js
+++ src/utils/pageExports.js
@@ -1,9 +1,9 @@
 // 從各自的文件導入頁面組件
 import Home from '../pages/home.jsx';
 import About from '../Pages/about.jsx'; // 修正了文件名的大小寫問題
-import Blog from '../pages/blog.jsx';
-import Project from '../pages/project.jsx';
-import ProjectDetailPage from '../pages/ProjectDetailPage.jsx';
+import Blog from '../Pages/blog.jsx';
+import Project from '../Pages/project.jsx';
+import ProjectDetailPage from '../Pages/ProjectDetailPage.jsx';
 
 // 導出所有頁面組件
 export { Home, About, Blog, Project, ProjectDetailPage }; 
