8cbe6462fd4e6c6871cb9e4eb952b8f90dae5031
kakeru13468 <auchergod@gmail.com>
2025-08-30 14:09:40 +0800

    fix: fix router erroe

--- src/utils/pageExports.js
+++ src/utils/pageExports.js
@@ -1,9 +1,9 @@
 // 從各自的文件導入頁面組件
-import Home from '../pages/home';
-import About from '../Pages/about'; // 修正了文件名的大小寫問題
-import Blog from '../pages/blog';
-import Project from '../pages/project';
-import ProjectDetailPage from '../pages/ProjectDetailPage';
+import Home from '../pages/home.jsx';
+import About from '../Pages/about.jsx'; // 修正了文件名的大小寫問題
+import Blog from '../pages/blog.jsx';
+import Project from '../pages/project.jsx';
+import ProjectDetailPage from '../pages/ProjectDetailPage.jsx';
 
 // 導出所有頁面組件
 export { Home, About, Blog, Project, ProjectDetailPage }; 
